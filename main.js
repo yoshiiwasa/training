9ffde668eab0ca8fc948ad165e91cb4ed84e238a
m.okamoto <m.okamoto@cloud9-inc.net>
2025-12-15 16:07:15 +0900

    進捗#7まで

---
+++ main.js
@@ -0,0 +1,26 @@
+'use strict'
+
+{
+    function showHeader() {
+        console.log('Header');
+    }
+
+    const showUsers = async () => {
+        try {
+        const response = await fetch('https://dotinstall.github.io/setup/fetchapi/users.json');
+        const users = await response.json();
+        console.log(users);
+    } catch (err){
+        console.log('Something went wrong getting user data');
+        console.log('Error log: ' + err);
+    }    
+    }
+
+    function showFooter() {
+        console.log('Footer');
+    }
+
+    showHeader();
+    showUsers();
+    showFooter();
+}
